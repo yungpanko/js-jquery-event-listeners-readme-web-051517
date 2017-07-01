57a8e4f53a502149eac044a6d5e0d8af0aed3b93
yungpanko <jrjohns@mit.edu>
2017-07-01 19:54:02 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+function getIt() {
+  $('p').on("click", () => {
+    alert("Hey!")
+  })
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').on("load", () => {
+    $('img').addClass("tasty")
+  })
+}
 
-// call functions here
+function pressIt() {
+  $('#typing').on("keydown", (key) => {
+    if (key.which === 71) {
+      alert("g was pressed")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", () => {
+    alert("Your form is going to be submitted now.")
+  })
+
+}
+
+$(document).ready(function () {
+
+  // call functions here
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 
 });
